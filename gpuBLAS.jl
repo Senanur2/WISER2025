6ec9f293edc9c1cfd0d420fcb576a57c4ebb92f8
Senanur2 <159006641+Senanur2@users.noreply.github.com>
2025-07-31 11:36:40 +0300

    Update gpuBLAS.jl

--- gpuBLAS.jl
+++ gpuBLAS.jl
@@ -7,3 +7,16 @@ function vector_add_kernel(a, b, c)
     end
     return
 end
+
+N = 1000
+a = CUDA.fill(1.0f0, N)
+b = CUDA.fill(2.0f0, N)
+c = CUDA.zeros(Float32, N)
+
+threads_per_block = 256
+blocks = cld(N, threads_per_block)
+
+@cuda threads=threads_per_block blocks=blocks vector_add_kernel(a, b, c)
+
+result = Array(c)
+println(result[1:10])
