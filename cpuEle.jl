883afb6cac97ecb11a20d629d8e86dec9e719b9e
Senanur2 <159006641+Senanur2@users.noreply.github.com>
2025-08-09 16:49:54 +0300

    Update cpuEle.jl

--- cpuEle.jl
+++ cpuEle.jl
@@ -9,15 +9,13 @@ end
 n = parse(Int, ARGS[1])
 thread_count = parse(Int, ARGS[2])
 
-# Make sure to run Julia with correct threads:
-# JULIA_NUM_THREADS=thread_count julia benchmark.jl n thread_count
+println("Julia threads: ", Threads.nthreads())  # should match thread_count if JULIA_NUM_THREADS set correctly
 
 A = randn(n, n)
 B = randn(n, n)
 C_builtin = zeros(n, n)
 C_elementwise = zeros(n, n)
 
-# Element-wise for-loop multiply with threading
 function elementwise_forloop!(C, A, B)
     @inbounds @threads for i in 1:size(A, 1)
         for j in 1:size(A, 2)
@@ -26,33 +24,27 @@ function elementwise_forloop!(C, A, B)
     end
 end
 
-# GFLOPS for matrix multiply (2 * n^3 FLOPs)
 function gflops_matrix(n, time_s)
     flops = 2 * n^3
     return flops / (time_s * 1e9)
 end
 
-# GFLOPS for element-wise multiply (n^2 FLOPs)
 function gflops_elementwise(n, time_s)
     flops = n^2
     return flops / (time_s * 1e9)
 end
 
-# Benchmarks
-
 r_builtin = @benchmark $C_builtin = $A * $B samples=5 evals=1
 builtin_time = minimum(r_builtin).time / 1e9
 builtin_gflops = gflops_matrix(n, builtin_time)
 
-r_elementwise_loop = @benchmark elementwise_forloop!($C_elementwise, $A, $B) samples=5 evals=1
-elementwise_time = minimum(r_elementwise_loop).time / 1e9
+r_elementwise = @benchmark elementwise_forloop!($C_elementwise, $A, $B) samples=5 evals=1
+elementwise_time = minimum(r_elementwise).time / 1e9
 elementwise_gflops = gflops_elementwise(n, elementwise_time)
 
-# Accuracy check
 diff_elementwise = maximum(abs.(C_elementwise .- (A .* B)))
 
-# Output
-println("Matrix size: $n x $n")
+println("\nMatrix size: $n x $n")
 println("Threads used: $thread_count\n")
 
 println("Built-in A * B:")
